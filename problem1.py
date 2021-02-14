fcde0ae5c74a01caa395db163cbac90f85adb487
Andrea Yeh <andrea.yeh23@ycdsbk12.ca>
2021-02-14 18:57:34 +0000

    finished 1 and 2

--- problem1.py
+++ problem1.py
@@ -1,1 +1,30 @@
+"""
+----------------------------------------------------------
+Name: problem1.py
+Purpose: Allows user to enter the three side lengths of a triangle and determine if it is a right-angled triangle or not.
 
+Author: Yeh.A
+
+Created: 14/02/2021
+----------------------------------------------------------
+"""
+
+print("***** Is it a Right Triangle or Not? *****")
+print(" ")
+
+#get side lengths
+side_1 = int(input("Enter the length of side 1: "))
+side_2 = int(input("Enter the length of side 2: "))
+side_3 = int(input("Enter the length of side 3: "))
+
+#compute squares
+square_1 = side_1**2
+square_2 = side_2**2
+square_3 = side_3**2
+
+#use pythagorean theorem to determine if it's a right triangle
+if (square_1 + square_2 == square_3) or (square_1 + square_3 == square_2) or (square_2 + square_3 == square_1):
+  print("A triangle with side lengths " + str(side_1) + ", " + str(side_2) + ", and " + str(side_3) + " forms a right-angled triangle.")
+
+else:
+  print("A triangle with side lengths " + str(side_1) + ", " + str(side_2) + ", and " + str(side_3) + " does not form a right-angled triangle.")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,28 @@
+"""
+----------------------------------------------------------
+Name: problem2.py
+Purpose: Allows you to enter your summer earnings and average grade to determine where you will be going this summer!
 
+Author: Yeh.A
+
+Created: 14/02/2021
+----------------------------------------------------------
+"""
+print("***** Where Will You Go This Summer? *****")
+print(" ")
+
+#get average grade and earnings before the summer
+mark = float(input("Enter your average grade: "))
+earnings_before_the_summer = float(input("Enter your earnings before the summer ($): "))
+
+#if mark is higher than 80 and you earned $500 or more it outputs you can go to Europe
+if mark >= 80 and earnings_before_the_summer >= 500:
+  print("You can go away to Europe!")
+
+#if mark is higher than 80 but you didn't earn $500 or more it outputs you can go to California
+elif mark >= 80:
+  print("You can go to California!")
+
+#if you didn't get an average of higher than 80 it outputs you can't go anywhere
+else: 
+  print("You don't get to go away.")
